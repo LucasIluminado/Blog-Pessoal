0f7a8b2651faac530bb5a17fd4daa0c7c7627f51
LucasIluminado <lucask72002@gmail.com>
2021-11-26 11:51:07 -0300

    Implementando REACTJS no blog pessoal

---
+++ blog-pessoal/src/paginas/home/Home.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './Home.css';
+
+function Home(){
+    return(
+        <>
+            <h1 className='titulo'>Home</h1>
+            <img src="https://camo.githubusercontent.com/f23154b22ee6fd7aa207d3cb7100b3e17cfa75d6a7d2e661d979d37b87e74350/68747470733a2f2f692e7069636173696f6e2e636f6d2f70696339312f38326336336464363539666431336564343637373533633264316631373430332e676966"
+            alt="Git Hub" className='img' />
+        </>
+    );
+}
+
+export default Home;
+    
+
